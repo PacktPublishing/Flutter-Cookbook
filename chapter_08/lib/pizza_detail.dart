055244967c71020af6a0a1bf9580f75ffbd8ae7f
Shubham Sharma <60926838+shubham763@users.noreply.github.com>
2021-05-21 14:18:08 +0530

    Update pizza_detail.dart

--- chapter_08/lib/pizza_detail.dart
+++ chapter_08/lib/pizza_detail.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'pizza.dart';
+
 import 'httphelper.dart';
+import 'pizza.dart';
 
 class PizzaDetail extends StatefulWidget {
-  final Pizza pizza;
-  final bool isNew;
-  PizzaDetail(this.pizza, this.isNew);
   @override
   _PizzaDetailState createState() => _PizzaDetailState();
 }
@@ -17,19 +15,6 @@ class _PizzaDetailState extends State<PizzaDetail> {
   final TextEditingController txtPrice = TextEditingController();
   final TextEditingController txtImageUrl = TextEditingController();
   String postResult = '';
-
-  @override
-  void initState() {
-    if (!widget.isNew) {
-      txtId.text = widget.pizza.id.toString();
-      txtName.text = widget.pizza.pizzaName;
-      txtDescription.text = widget.pizza.description;
-      txtPrice.text = widget.pizza.price.toString();
-      txtImageUrl.text = widget.pizza.imageUrl;
-    }
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,60 +24,63 @@ class _PizzaDetailState extends State<PizzaDetail> {
         body: Padding(
             padding: EdgeInsets.all(12),
             child: SingleChildScrollView(
-              child: Column(children: [
-                Text(
-                  postResult,
-                  style: TextStyle(
-                      backgroundColor: Colors.green[200], color: Colors.black),
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                TextField(
-                  controller: txtId,
-                  decoration: InputDecoration(hintText: 'Insert ID'),
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                TextField(
-                  controller: txtName,
-                  decoration: InputDecoration(hintText: 'Insert Pizza Name'),
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                TextField(
-                  controller: txtDescription,
-                  decoration: InputDecoration(hintText: 'Insert Description'),
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                TextField(
-                  controller: txtPrice,
-                  decoration: InputDecoration(hintText: 'Insert Price'),
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                TextField(
-                  controller: txtImageUrl,
-                  decoration: InputDecoration(hintText: 'Insert Image Url'),
-                ),
-                SizedBox(
-                  height: 48,
-                ),
-                ElevatedButton(
-                    child: Text('Send Post'),
-                    onPressed: () {
-                      savePizza();
-                    })
-              ]),
+              child: Column(
+                children: [
+                  Text(
+                    postResult,
+                    style: TextStyle(
+                        backgroundColor: Colors.green[200],
+                        color: Colors.black),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  TextField(
+                    controller: txtId,
+                    decoration: InputDecoration(hintText: 'Insert ID'),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  TextField(
+                    controller: txtName,
+                    decoration: InputDecoration(hintText: 'Insert Pizza Name'),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  TextField(
+                    controller: txtDescription,
+                    decoration: InputDecoration(hintText: 'Insert Description'),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  TextField(
+                    controller: txtPrice,
+                    decoration: InputDecoration(hintText: 'Insert Price'),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  TextField(
+                    controller: txtImageUrl,
+                    decoration: InputDecoration(hintText: 'Insert Image Url'),
+                  ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  ElevatedButton(
+                      child: Text('Send Post'),
+                      onPressed: () {
+                        postPizza();
+                      })
+                ],
+              ),
             )));
   }
 
-  Future savePizza() async {
+  Future postPizza() async {
     HttpHelper helper = HttpHelper();
     Pizza pizza = Pizza();
     pizza.id = int.tryParse(txtId.text);
@@ -100,17 +88,10 @@ class _PizzaDetailState extends State<PizzaDetail> {
     pizza.description = txtDescription.text;
     pizza.price = double.tryParse(txtPrice.text);
     pizza.imageUrl = txtImageUrl.text;
-   String result = '';
-    if (widget.isNew) {
-      result = await helper.postPizza(pizza);
-    } else {
-      result = await helper.putPizza(pizza);
-    }
+    String result = await helper.postPizza(pizza);
     setState(() {
       postResult = result;
     });
     return result;
   }
-
-
 }
